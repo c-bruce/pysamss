08a538bb7124f6505af801752961d9a3630ac5a7
c-bruce <callum.bruce1@gmail.com>
2019-06-12 21:37:09 +0100

    System class update.

--- rocketsim/main/system.py
+++ rocketsim/main/system.py
@@ -13,6 +13,51 @@ class System:
         celestial_bodies (list): List of CelestialBody objects in system.
         vessels (list): List of Vessel objects in system.
     """
-    def __init__(self, celestial_bodies, vessels):
-        self.celestial_bodies = celestial_bodies
-        self.vessels = vessels
+    def __init__(self, celestial_bodies=None, vessels=None):
+        if celestial_bodies == None:
+            self.celestial_bodies = []
+        else:
+            self.celestial_bodies = celestial_bodies
+        if vessels == None:
+            self.vessels = []
+        else:
+            self.vessels = vessels
+        self.time = [0]
+        self.endtime = 100
+        self.dt = 0.1
+
+    def set_endtime(self, endtime):
+        """
+        Set system end time [s].
+        """
+        self.endtime = endtime
+
+    def set_dt(self, dt):
+        """
+        Set timestep, dt [s].
+        """
+        self.dt = dt
+
+    def addCelestialBody(self, celestial_body):
+        """
+        Add a CelestialBody to the system.
+        """
+        self.celestial_bodies.append(celestial_body)
+
+    def addVessel(self, vessel):
+        """
+        Add a Vessel to the system.
+        """
+        self.vessels.append(vessel)
+
+    def save(self, path):
+        """
+        Save system to file
+        ##### .h5 or text #####
+        """
+
+    def load(self, path):
+        """
+        Save system to file
+        ##### .h5 or text #####
+        """
