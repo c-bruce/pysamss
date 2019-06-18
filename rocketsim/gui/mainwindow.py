8d67512f7774e06371ffead8f7baec5f6b0db5c6
c-bruce <callum.bruce1@gmail.com>
2019-06-18 18:11:36 +0100

    MainWindow update.

--- rocketsim/gui/mainwindow.py
+++ rocketsim/gui/mainwindow.py
@@ -4,7 +4,8 @@
 # Main event loop
 from mayavi_qwidget import MayaviQWidget
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QHBoxLayout, QSplitter, QTreeView, QWidget, QPushButton, QTabWidget
 
 import sys
 
@@ -16,14 +17,36 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("RocketSim")
 
-        layout = QVBoxLayout()
-        layout.addWidget(MayaviQWidget())
+        layout_top = QHBoxLayout()
+        layout_top.addWidget(QPushButton("Button 1"), 1)
+        layout_top.addWidget(QPushButton("Button 2"), 2)
+
+        layout_middle = MiddleWidget()
+        '''
+        layout_middle = QSplitter()
+        layout_middle.setOrientation(Qt.Horizontal)
+        layout_middle.addWidget(QTreeView())
+        layout_middle.addWidget(MayaviQWidget())
+        '''
+        layout_bottom = QHBoxLayout()
+        layout_bottom.addWidget(QPushButton(), 1)
+
+        layout_main = QVBoxLayout()
+        layout_main.addLayout(layout_top, 1)
+        layout_main.addWidget(layout_middle, 2)
+        layout_main.addLayout(layout_bottom, 3)
 
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(layout_main)
 
         self.setCentralWidget(widget) # Set the central widget of the Window.
 
+class MiddleWidget(QSplitter):
+    def __init__(self, *args, **kwargs):
+        super(MiddleWidget, self).__init__(*args, **kwargs)
+        self.setOrientation(Qt.Horizontal)
+        self.addWidget(QTreeView())
+        self.addWidget(MayaviQWidget())
 # Main loop
 if __name__ == '__main__':
     app = QApplication(sys.argv)
