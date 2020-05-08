504ce93f75c9235dedad1a0c309ba24185b36d27
c-bruce <callum.bruce1@gmail.com>
2020-05-08 17:21:29 +0100

    setup.py and examples folder added

--- examples/EarthMoonISS_example.py
+++ examples/EarthMoonISS_example.py
@@ -4,24 +4,23 @@
 # To run, ensure 'de430.bsp' is downloaded to the working directory. See https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/.
 import numpy as np
 from mayavi import mlab
-from pysamss import *
 import datetime
-import julian
 from jplephem.spk import SPK
 import urllib3
 from sgp4.api import Satrec
+import pysamss
 
 # Step 1: Setup system
-system = System('EarthMoonISS')
+system = pysamss.System('EarthMoonISS')
 system.current.setDatetime(datetime.datetime.utcnow()) # Set current time utc
 # Step 1.1: Add Earth, Moon and ISS to system
-system.current.addCelestialBody(CelestialBody('Earth', 5.972e24, 6.371e6))
-system.current.addCelestialBody(CelestialBody('Moon', 7.348e22, 1.737e6, parent_name='Earth'))
-system.current.addVessel(Vessel('ISS', [Stage(419725, 1, 10, np.array([0, 0, 0]))], parent_name='Earth'))
+system.current.addCelestialBody(pysamss.CelestialBody('Earth', 5.972e24, 6.371e6))
+system.current.addCelestialBody(pysamss.CelestialBody('Moon', 7.348e22, 1.737e6, parent_name='Earth'))
+system.current.addVessel(pysamss.Vessel('ISS', [pysamss.Stage(419725, 1, 10, np.array([0, 0, 0]))], parent_name='Earth'))
 
 # Step 2: Calculate positions and velocities
 time = system.current.getJulianDate()
-kernel = SPK.open('pysamss/resources/de430.bsp')
+kernel = SPK.open('de430.bsp')
 earth_pos, earth_vel = kernel[3,399].compute_and_differentiate(time)
 # Earth
 earth_pos *= 1000 # Convert from km -> m
@@ -49,11 +48,11 @@ iss_vel = np.array(iss_vel) * 1000
 system.current.celestial_bodies['Earth'].setPosition(earth_pos)
 system.current.celestial_bodies['Earth'].setVelocity(earth_vel)
 system.current.celestial_bodies['Earth'].setAttitudeDot(np.array([0.0, 0.0, np.deg2rad(360 / ((23 * 60 * 60) + (56 * 60) + 4))]))
-system.current.celestial_bodies['Earth'].setTexture('pysamss/resources/earth.jpg')
+system.current.celestial_bodies['Earth'].setTexture(pysamss.__file__[:-12] + '/resources/earth.jpg')
 system.current.celestial_bodies['Moon'].setPosition(moon_pos)
 system.current.celestial_bodies['Moon'].setVelocity(moon_vel)
 system.current.celestial_bodies['Moon'].setAttitudeDot(np.array([0.0, 0.0, np.deg2rad(360 / 2358720.0)]))
-system.current.celestial_bodies['Moon'].setTexture('pysamss/resources/moon.jpg')
+system.current.celestial_bodies['Moon'].setTexture(pysamss.__file__[:-12] + '/resources/moon.jpg')
 system.current.vessels['ISS'].setPosition(iss_pos, local=True)
 system.current.vessels['ISS'].setVelocity(iss_vel, local=True)
 
@@ -65,7 +64,7 @@ system.simulateSystem()
 
 # Step 5: Post processing
 system.load('EarthMoonISS.psm')
-fig = MainWidget()
+fig = pysamss.MainWidget()
 fig.loadSystem(system)
 fig.showMaximized()
 mlab.show()
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysamss',
+      version='0.0.1',
+      description='Python Space Asset Management and Simulation System',
+      author='Callum Bruce',
+      author_email='callum.bruce1@gmail.com',
+      url='https://github.com/c-bruce/pysamss',
+      packages=['pysamss',
+                'pysamss.resources',
+                'pysamss.main',
+                'pysamss.gui',
+                'pysamss.helpermath',
+                'pysamss.control',
+                'pysamss.plotting',
+                'pysamss.forcetorque'],
+      package_data={'pysamss.resources' : ['*.jpg']},
+      install_requires=['h5py>=2.10.0',
+                        'jplephem>=2.14',
+                        'julian>=0.14',
+                        'mayavi>=4.7.1',
+                        'numpy>=1.18.1',
+                        'PyQt5>=5.14.2',
+                        'sgp4>=2.7']
+     )
